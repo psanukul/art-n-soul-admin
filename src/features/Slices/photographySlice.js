8c7c7eca4967e8095b1397730866836f50904c44
psanukul <anukulsaxena@pearlorganisation.com>
2024-11-05 14:12:41 +0530

    update photography

--- src/features/Slices/photographySlice.js
+++ src/features/Slices/photographySlice.js
@@ -1,119 +1,125 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "sonner";
 import { redirect } from "react-router-dom";
-import {CreatePhotography, getDataById, GetPhotography, updatePhotography } from "../actions/photographyAction";
+import {
+  CreatePhotography,
+  getDataById,
+  GetPhotography,
+  updatePhotography,
+  uploadPhotographyImages,
+} from "../actions/photographyAction";
 
+const initialState = {
+  isLoading: false,
+  isSuccess: false,
+  photographyData: [],
+  errorMessage: "",
+  isdeleted: false,
+};
 
- const initialState={
-    isLoading:false,
-    isSuccess:false,
-    photographyData:[],
-    errorMessage:"",
-    isdeleted:false,
-}
-
-export  const  photographySlice=createSlice({
-    name:"photographySlice",
-    initialState,
-    reducers:{
-        clearIsSuccess:(state)=>{
-            state.isSuccess=false,
-            state.isdeleted=false
-        }
+export const photographySlice = createSlice({
+  name: "photographySlice",
+  initialState,
+  reducers: {
+    clearIsSuccess: (state) => {
+      (state.isSuccess = false), (state.isdeleted = false);
     },
-    extraReducers:(builder)=>{
-        builder
-        .addCase(CreatePhotography.pending,(state,action)=>{
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(CreatePhotography.pending, (state, action) => {
+        (state.isLoading = true),
+          (state.isSuccess = false),
+          (state.errorMessage = "");
+      })
+      .addCase(CreatePhotography.fulfilled, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = true),
+          (state.errorMessage = ""),
+          (state.photographyData = action.payload);
+      })
+      .addCase(CreatePhotography.rejected, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = false),
+          toast.error(action?.payload || "Something went wrong", {
+            position: "top-center",
+          });
+      })
+      .addCase(GetPhotography.pending, (state, action) => {
+        (state.isLoading = true),
+          (state.isSuccess = false),
+          (state.errorMessage = "");
+      })
+      .addCase(GetPhotography.fulfilled, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = true),
+          (state.errorMessage = ""),
+          (state.photographyData = action.payload);
+      })
+      .addCase(GetPhotography.rejected, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = false),
+          toast.error(action?.payload || "Something went wrong", {
+            position: "top-center",
+          });
+      })
 
-            state.isLoading=true,
-            state.isSuccess=false,
-            state.errorMessage=""
-        })
-        .addCase(CreatePhotography.fulfilled,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=true,
-            state.errorMessage="",
-            state. photographyData=action.payload       })
-        .addCase(CreatePhotography.rejected,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=false,
-            toast.error(action?.payload || "Something went wrong",{
-                position:"top-center"
-              })
-          
-        })
-        .addCase(GetPhotography.pending,(state,action)=>{
+      .addCase(getDataById.pending, (state, action) => {
+        (state.isLoading = true),
+          (state.isSuccess = false),
+          (state.errorMessage = "");
+      })
+      .addCase(getDataById.fulfilled, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = true),
+          (state.errorMessage = ""),
+          (state.photographyData = action.payload);
+      })
+      .addCase(getDataById.rejected, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = false),
+          toast.error(action?.payload || "Something went wrong", {
+            position: "top-center",
+          });
+      })
+      .addCase(updatePhotography.pending, (state, action) => {
+        (state.isLoading = true),
+          (state.isSuccess = false),
+          (state.errorMessage = "");
+      })
+      .addCase(updatePhotography.fulfilled, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = true),
+          (state.errorMessage = ""),
+          (state.photographyData = action.payload);
+      })
+      .addCase(updatePhotography.rejected, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = false),
+          toast.error(action?.payload || "Something went wrong", {
+            position: "top-center",
+          });
+      })
 
-            state.isLoading=true,
-            state.isSuccess=false,
-            state.errorMessage=""
-        })
-        .addCase(GetPhotography.fulfilled,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=true,
-            state.errorMessage="",
-            state. photographyData=action.payload       })
-        .addCase(GetPhotography.rejected,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=false,
-            toast.error(action?.payload || "Something went wrong",{
-                position:"top-center"
-              })
-
-
-              
-        })
-
-        .addCase(getDataById.pending,(state,action)=>{
-
-            state.isLoading=true,
-            state.isSuccess=false,
-            state.errorMessage=""
-        })
-        .addCase(getDataById.fulfilled,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=true,
-            state.errorMessage="",
-            state. photographyData=action.payload       })
-        .addCase(getDataById.rejected,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=false,
-            toast.error(action?.payload || "Something went wrong",{
-                position:"top-center"
-              })
-             
-        })
-
-        
-        .addCase(updatePhotography.pending,(state,action)=>{
-
-            state.isLoading=true,
-            state.isSuccess=false,
-            state.errorMessage=""
-        })
-        .addCase(updatePhotography.fulfilled,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=true,
-            state.errorMessage="",
-            state. photographyData=action.payload       })
-        .addCase(updatePhotography.rejected,(state,action)=>{
-            state.isLoading=false,
-            state.isSuccess=false,
-            toast.error(action?.payload || "Something went wrong",{
-                position:"top-center"
-              })
-
-
-              
-        })
-
-
-    }
-
-
-})
-
-
+      .addCase(uploadPhotographyImages.pending, (state, action) => {
+        (state.isLoading = true),
+          (state.isSuccess = false),
+          (state.errorMessage = "");
+      })
+      .addCase(uploadPhotographyImages.fulfilled, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = true),
+          (state.errorMessage = "");
+      })
+      .addCase(uploadPhotographyImages.rejected, (state, action) => {
+        (state.isLoading = false),
+          (state.isSuccess = false),
+          toast.error(action?.payload || "Something went wrong", {
+            position: "top-center",
+          });
+      });
+  },
+});
 
 export const { clearIsSuccess } = photographySlice.actions;
 export default photographySlice.reducer;
--- src/features/store.js
+++ src/features/store.js
@@ -1,17 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import galleryReducer from './Slices/photographySlice'; // Adjust the path based on your folder structure
-import FilmsSlices from './Slices/FilmsSlices';
-import photographySlice from './Slices/photographySlice';
+import { configureStore } from "@reduxjs/toolkit";
+import galleryReducer from "./Slices/photographySlice"; // Adjust the path based on your folder structure
+import FilmsSlices from "./Slices/FilmsSlices";
+import photographySlice from "./Slices/photographySlice";
 
 const store = configureStore({
   reducer: {
     photography: photographySlice,
-    Films:FilmsSlices
-
-    
-      
-
-
+    Films: FilmsSlices,
   },
 });
 
