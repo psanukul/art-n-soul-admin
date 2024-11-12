4480820f2dcdd1435e787244cdd3d6d97915d0e6
psanukul <anukulsaxena@pearlorganisation.com>
2024-11-12 17:09:13 +0530

    check

--- src/pages/Photography/AddPhotography.jsx
+++ src/pages/Photography/AddPhotography.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { ClipLoader } from "react-spinners";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   CreatePhotography,
   getDataById,
