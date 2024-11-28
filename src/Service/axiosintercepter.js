7d1f5ac2c3f6d2fb24aa2a584b923e7b61f59383
psanukul <anukulsaxena@pearlorganisation.com>
2024-11-28 17:05:41 +0530

    ok

--- src/Service/axiosintercepter.js
+++ src/Service/axiosintercepter.js
@@ -9,11 +9,7 @@ export const injectStore = (_store) => {
 // Creating new axios instance
 export const instance = axios.create({
   withCredentials: true,
-  baseURL: `${
-    import.meta.env.VITE_REACT_APP_WORKING_ENVIRONMENT === "development"
-      ? import.meta.env.VITE_REACT_APP_API_BASE_URL_DEVELOPMENT
-      : import.meta.env.VITE_REACT_APP_API_BASE_URL_MAIN_PRODUCTION
-  }`,
+  baseURL: `https://art-n-soul-backend.onrender.com/api/v1`,
 });
 
 instance.interceptors.response.use(
